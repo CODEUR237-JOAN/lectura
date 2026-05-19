1dfa53b9e0a6ab213e0c73f3690cb2fc392ffdf1
Joan Dongmo <joanfreeddonfmomeufo@gmail.com>
2026-05-19 13:07:55 +0100

    Fix: Add root index bypass for Nixpacks 403 restriction

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// Ce fichier permet de simuler la fonctionnalité "mod_rewrite" d'Apache.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
